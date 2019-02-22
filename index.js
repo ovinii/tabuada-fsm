8f97065f55b1333286ceae7b613c3c0ae280c155
Vinícius Nascimento <39114511+viniciusnaascimento@users.noreply.github.com>
2019-02-22 20:50:38 -0300

    init
    Learn fullstack.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+const indexRouter = require('./routes/index')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use('/', indexRouter)
+
+app.listen(port, () => console.log('Tabuada server on port: ' + port))
